f8d8509fffc1a5f8027badb247e35436dbb057aa
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-07 22:36:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,17 @@
 class Dog
-attr_accessor :owner, :mood
-attr_reader :name
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all=[]
 
   def initialize(name, owner, mood="nervous")
     @name=name
     @owner=owner
     @mood=mood
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
   end
 
 end
